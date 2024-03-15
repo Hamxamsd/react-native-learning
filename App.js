6932e0e8a9c142c16c50d5d7a18795075d0075e1
Hamxamsd <Ameeerhamxakhan@gmail.com>
2024-03-15 15:39:27 +0500

    flex

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import React from "react";
-import { SectionList, Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 const App = () => {
-  const users = [
-    {
-      id: 1,
-      name: "John",
-      data: ["JS", "JAVA", "REACT-JS"],
-    },
-    {
-      id: 2,
-      name: "Bruce",
-      data: ["PHP", "JAVA", "REACT-Native"],
-    },
-    {
-      id: 3,
-      name: "Doe",
-      data: ["CPP", "Python", "Laravel"],
-    },
-  ];
-
   return (
     <>
-      <View>
+      <View style={{ flex: 1 }}>
         <View style={{ backgroundColor: "gray", width: "100%", height: 30 }} />
-        <View style={{ alignItems: "center", backgroundColor: "purple" }}>
+        <View
+          style={{ alignItems: "center", backgroundColor: "purple", flex: 1 }}
+        >
           <Text
             style={{
               fontSize: 22,
@@ -33,22 +17,10 @@ const App = () => {
               marginY: 5,
             }}
           >
-            Section List in React Native
+            Responsive Design in React Native
           </Text>
         </View>
-        <SectionList
-          sections={users}
-          renderItem={({ item }) => (
-            <Text style={{ fontSize: 17, color: "gray", marginLeft: 10 }}>
-              {item}
-            </Text>
-          )}
-          renderSectionHeader={({ section: { name } }) => (
-            <Text style={{ fontSize: 20, fontWeight: "bold", color: "blue" }}>
-              {name}
-            </Text>
-          )}
-        />
+        <Button title="Button" />
       </View>
     </>
   );
