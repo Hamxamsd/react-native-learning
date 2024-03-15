9dd3b779754dffe03458cff7c1dadc814239a854
Hamxamsd <Ameeerhamxakhan@gmail.com>
2024-03-15 14:12:50 +0500

    SectionList

--- App.js
+++ App.js
@@ -1,35 +1,53 @@
 import React from "react";
-import { Text, View, FlatList, StyleSheet } from "react-native";
+import { SectionList, Text, View } from "react-native";
 
 const App = () => {
   const users = [
-    { name: "Hamza", id: 1, email: "hamza@gmail.com" },
-    { name: "Mansoor", id: 2, email: "mansoor@gmail.com" },
-    { name: "Najeeb", id: 3, email: "najeeb@gmail.com" },
-    { name: "Waqar", id: 4, email: "waqar@gmail.com" },
-    { name: "Rohail", id: 5, email: "rohail@gmail.com" },
-    { name: "Sheroo", id: 6, email: "sheroo@gmail.com" },
-    { name: "Zabih", id: 7, email: "zabih@gmail.com" },
-    { name: "Qadu", id: 8, email: "qadu@gmail.com" },
-    { name: "Ghafoor", id: 9, email: "ghafoor@gmail.com" },
-    { name: "Sohaib", id: 10, email: "sohib@gmail.com" },
-    { name: "MPA", id: 11, email: "MPA@gmail.com" },
-    { name: "Hanzla", id: 12, email: "hanzla@gmail.com" },
-    { name: "Asad", id: 13, email: "asad@gmail.com" },
-    { name: "Faisal", id: 14, email: "faisal@gmail.com" },
-    { name: "Mama", id: 15, email: "mama@gmail.com" },
+    {
+      id: 1,
+      name: "John",
+      data: ["JS", "JAVA", "REACT-JS"],
+    },
+    {
+      id: 2,
+      name: "Bruce",
+      data: ["PHP", "JAVA", "REACT-Native"],
+    },
+    {
+      id: 3,
+      name: "Doe",
+      data: ["CPP", "Python", "Laravel"],
+    },
   ];
+
   return (
     <>
       <View>
-        <View style={{ alignItems: "center" }}>
-          <Text style={{ fontSize: 22, marginTop: 40, fontWeight: "900" }}>
-            Component in Loop with FlatList
+        <View style={{ backgroundColor: "gray", width: "100%", height: 30 }} />
+        <View style={{ alignItems: "center", backgroundColor: "purple" }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: "900",
+              color: "white",
+              marginY: 5,
+            }}
+          >
+            Section List in React Native
           </Text>
         </View>
-        <FlatList
-          data={users}
-          renderItem={(item) => <UserData data={item} />}
+        <SectionList
+          sections={users}
+          renderItem={({ item }) => (
+            <Text style={{ fontSize: 17, color: "gray", marginLeft: 10 }}>
+              {item}
+            </Text>
+          )}
+          renderSectionHeader={({ section: { name } }) => (
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: "blue" }}>
+              {name}
+            </Text>
+          )}
         />
       </View>
     </>
@@ -37,29 +55,3 @@ const App = () => {
 };
 
 export default App;
-
-const UserData = ({ data }) => {
-  const { item } = data;
-  return (
-    <View style={styles.box}>
-      <Text style={styles.item}>{item.name}</Text>
-      <Text style={styles.item}>{item.email}</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  item: {
-    fontSize: 15,
-    color: "purple",
-    textAlign: "center",
-    textAlignVertical: "center",
-    flex: 1,
-  },
-  box: {
-    flexDirection: "row",
-    borderColor: "purple",
-    borderWidth: 2,
-    margin: 5,
-  },
-});
