62ba69ca0b9b78a199502371e5d8842e588b4500
Hamxamsd <Ameeerhamxakhan@gmail.com>
2024-03-15 11:26:10 +0500

    Maping with ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, ScrollView } from "react-native";
 import ExStyles from "./style";
 
 const App = () => {
@@ -8,6 +8,17 @@ const App = () => {
     { name: "Mansoor", id: 2 },
     { name: "Najeeb", id: 3 },
     { name: "Waqar", id: 4 },
+    { name: "Rohail", id: 5 },
+    { name: "Sheroo", id: 6 },
+    { name: "Zabih", id: 7 },
+    { name: "Qadu", id: 8 },
+    { name: "Ghafoor", id: 9 },
+    { name: "Sohaib", id: 10 },
+    { name: "MPA", id: 11 },
+    { name: "Hanzla", id: 12 },
+    { name: "Asad", id: 13 },
+    { name: "Faisal", id: 14 },
+    { name: "Mama", id: 15 },
   ];
   return (
     <>
@@ -15,12 +26,11 @@ const App = () => {
         <Text style={{ fontSize: 30, marginTop: 40, alignItems: "center" }}>
           List of Flatlist Component
         </Text>
-        <FlatList
-          data={users}
-          renderItem={({ item }) => (
+        <ScrollView>
+          {users.map((item) => (
             <Text style={ExStyles.item}>{item.name}</Text>
-          )}
-        />
+          ))}
+        </ScrollView>
       </View>
     </>
   );
