25756a549466642f7c10ff3ce6f0425c6f1b7a3c
Hamxamsd <Ameeerhamxakhan@gmail.com>
2024-03-16 14:38:07 +0500

    Statusbar

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Text, View, StyleSheet, Pressable } from "react-native";
 
@@ -6,30 +7,12 @@ const App = () => {
 
   return (
     <>
-      <View style={styles.mainBox}>
-        <View style={{ backgroundColor: "gray", width: "100%", height: 30 }} />
-        <View style={{ alignItems: "center", backgroundColor: "purple" }}>
-          <Text
-            style={{
-              fontSize: 22,
-              fontWeight: "900",
-              color: "white",
-              marginBlock: 5,
-            }}
-          >
-            Pressable in React Native
-          </Text>
-        </View>
-        <View style={{ flex: 1, justifyContent: "center" }}>
-          <Pressable
-            // onPress={() => console.warn("Normal Press")}
-            // onLongPress={() => console.warn("Long Press")}
-            onPressIn={() => console.warn("Press In")}
-            onPressOut={() => console.warn("Press Out")}
-          >
-            <Text style={styles.btn}>Pressable</Text>
-          </Pressable>
-        </View>
+      <View>
+        <StatusBar
+          backgroundColor="black"
+          barStyle="light-content"
+          hidden={false}
+        />
       </View>
     </>
   );
@@ -40,16 +23,5 @@ export default App;
 const styles = StyleSheet.create({
   mainBox: {
     flex: 1,
-  },
-  btn: {
-    backgroundColor: "darkblue",
-    color: "white",
-    padding: 10,
-    margin: 10,
-    textAlign: "center",
-    fontWeight: "bold",
-    shadowColor: "#000",
-    elevation: 5,
-    borderRadius: 10,
   },
 });
