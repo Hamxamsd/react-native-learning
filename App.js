da33fe8860dea032e3bccb1ea804015d2de34916
Hamxamsd <Ameeerhamxakhan@gmail.com>
2024-03-16 13:44:36 +0500

    activityIndicator

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Button,
+} from "react-native";
 
 const App = () => {
-  const [selectedBtn, setSelectedBtn] = useState(1);
+  const [show, setShow] = useState(false);
 
   return (
     <>
@@ -17,26 +23,12 @@ const App = () => {
               marginBlock: 5,
             }}
           >
-            Custom Radio Buttons in React Native
+            ActivityIndicator in React Native
           </Text>
         </View>
-        <View style={styles.main}>
-          <TouchableOpacity onPress={() => setSelectedBtn(1)}>
-            <View style={styles.wrapper}>
-              <View style={styles.radio}>
-                {selectedBtn === 1 ? <View style={styles.radioBg} /> : null}
-              </View>
-              <Text style={styles.radioText}>Radio 1</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setSelectedBtn(2)}>
-            <View style={styles.wrapper}>
-              <View style={styles.radio}>
-                {selectedBtn === 2 ? <View style={styles.radioBg} /> : null}
-              </View>
-              <Text style={styles.radioText}>Radio 2</Text>
-            </View>
-          </TouchableOpacity>
+        <View>
+          <ActivityIndicator size={100} color="purple" animating={show} />
+          <Button title="loader" onPress={() => setShow(!show)} />
         </View>
       </View>
     </>
@@ -48,33 +40,5 @@ export default App;
 const styles = StyleSheet.create({
   mainBox: {
     flex: 1,
-  },
-  main: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  radioText: {
-    color: "black",
-    fontSize: 22,
-  },
-  radio: {
-    width: 40,
-    height: 40,
-    borderColor: "black",
-    borderRadius: 20,
-    borderWidth: 3,
-    margin: 10,
-  },
-  wrapper: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  radioBg: {
-    backgroundColor: "black",
-    height: 28,
-    width: 28,
-    borderRadius: 20,
-    margin: 3,
   },
 });
